e6ec06f0a5c9bbcad0d5234bbd08c8825f817a57
Diego Alejandro Arbeláez Vasquez <roboticsrionegro@gmail.com>
2026-01-19 22:03:49 -0500

    js-drum-key2

--- frontend/clase js/Ejercisios JS/Drum Kit Starting Files/index.js
+++ frontend/clase js/Ejercisios JS/Drum Kit Starting Files/index.js
@@ -29,15 +29,50 @@ for (let i =0;i<instrument;i++){
 button.style.height = "150px";
 button.style.backgroundImage = "url('" + images[i]+"')";*/
 
-button[i].addEventListener("click",function handleClik(){
+button[i].addEventListener("click",function(){
     const audio = new Audio(audios[i]);
     audio.play();
 });
 
 }
-document.addEventListener("keydown",function alerta(){
-    alert("se");
+document.addEventListener("keydown",function (event){
+    const llave = event.key;
+    let numSound = 0;
+    let bandera = 0;
+    switch (llave) {
+        case "w":
+            numSound=0;
+            break;
+        case "a":
+            numSound=1;
+            break
+        case "s":
+            numSound=2;
+            break
+        case "d":
+            numSound=3;
+            break
+        case "j":
+            numSound=4;
+            break
+        case "k":
+            numSound=5;
+            break
+        case "l":
+            numSound=6;
+            break
+        default:
+            bandera = 1;
+            break;
+    }
+    if (bandera===0){
+            const audio = new Audio(audios[numSound]);
+            audio.play();
+    }
+    
 })
+
+
 
 
 
